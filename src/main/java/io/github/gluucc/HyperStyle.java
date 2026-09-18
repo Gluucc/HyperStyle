8674090ca31ce90533369e5aed07bddaf703586d
Edgar Klepaylo <edgarklepaylo@gmail.com>
2026-09-19 02:38:25 +0500

    Migrate to Yarn mappings

--- src/main/java/io/github/gluucc/HyperStyle.java
+++ src/main/java/io/github/gluucc/HyperStyle.java
@@ -2,7 +2,7 @@ package io.github.gluucc;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +24,7 @@ public class HyperStyle implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 	}
 
-	public static ResourceLocation id(String path) {
-		return new ResourceLocation(MOD_ID, path);
+	public static Identifier id(String path) {
+		return new Identifier(MOD_ID, path);
 	}
 }
